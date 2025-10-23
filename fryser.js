0e0a7537bc7c6a7a65dd7e6f2828cea72d6bb83c
HappyDustbunny <madshorn@emailcorner.net>
2025-10-23 17:46:23 +0200

    Fetch content implemented

--- fryser.js
+++ fryser.js
@@ -1,9 +1,33 @@
-
-const content = new Map([
-  //[name, number, type, shelf, keepsInMonths, addedToFreezer]
-    ['Brød', [1, 'bread', 1, 5, 1761217795422]],
-    ['Kylling', [2, 'fowl', 2, 4, 1761217795422]]
+const content = new Map();
+const fixedContent = new Map([
+    //[name, number, type, shelf, keepsInMonths, addedToFreezer]
+    ['brød', [1, 'bread', 1, 5, 1761217795422]],
+    ['kylling', [2, 'fowl', 2, 4, 1761217795422]],
+    ['grønne bønner', [1, 'veggie', 2, 4, 1761217795422]]
 ])
+const symbols = new Map([
+    ['bread', '&#x1F35E;'],
+    ['veggie', '&#x1F346;'],
+    ['cake', '&#x1F382;'],
+    ['fish', '&#x1F41F;'],
+    ['fowl', '&#x1F414;'],
+    ['meat', '&#x1F969;']
+]);
+const allTab = document.getElementById('allTab');
+const typeTab = document.getElementById('typeTab');
+
+let localContent;
+
+class Item {
+    constructor(name, number, type, shelf, keepsInMonths, addedToFreezer) {
+        this.name = name;
+        this.number = number;
+        this.type = type;
+        this.shelf = shelf;
+        this.keepsInMonths = keepsInMonths;
+        this.addedToFreezer = addedToFreezer;
+    }
+}
 
 document.getElementById('type').addEventListener('click', typeButtonWasClicked);
 document.getElementById('all').addEventListener('click', allButtonWasClicked);
@@ -24,8 +48,31 @@ function allButtonWasClicked() {
 
 function setUpFunction() {
     allButtonWasClicked();
+    
+    // Get fixed content
+    fixedContent.forEach(function(value, key) {
+        console.log(key, value);
+        content.set(key, new Item(key, value[0], value[1], value[2], value[3], value[4]));
+    })
+    
+    // Get local content
+    if (localStorage.getItem('localContent')) {
+        localContent = JSON.parse(localStorage.localContent);
 
-    content.forEach(function(value, key) {
-        console.log(value, key);
+        localContent.forEach(function(value, key) {
+            content.set(key, new Item(key, value[0], value[1], value[2], value[3], value[4]));
+        })
+    }
+
+    fillAllTab();
+}
+
+function fillAllTab() {
+    let sortedContent = new Map([...content].sort());
+    sortedContent.forEach(function(value,key) {
+        let symbol = symbols.get(value.type);
+        let strippedKey = key.replace(/\s/g, '');
+        allTab.innerHTML += '<p id="' + strippedKey + '">' + symbol + ' ' + key[0].toUpperCase() + key.slice(1) + ' ' + 
+        ' ' + value.number + ' stk ' + ' i skuffe ' + value.shelf +  ' </p>'
     })
 }
