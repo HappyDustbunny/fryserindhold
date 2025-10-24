fb2d3a0293adbc78aa733bbcca745fb28c326d41
HappyDustbunny <madshorn@emailcorner.net>
2025-10-24 15:21:26 +0200

    Fix layout of items in show-all view and implement age colour coding

--- fryser.js
+++ fryser.js
@@ -3,7 +3,7 @@ const fixedContent = new Map([
     //[name, number, type, shelf, keepsInMonths, addedToFreezer]
     ['brød', [1, 'bread', 1, 5, 1761217795422]],
     ['kylling', [2, 'fowl', 2, 4, 1761217795422]],
-    ['grønne bønner', [1, 'veggie', 2, 4, 1761217795422]]
+    ['grønne bønner', [1, 'veggie', 2, 4, 1743717600000]]
 ])
 const symbols = new Map([
     ['bread', '&#x1F35E;'],
@@ -68,11 +68,32 @@ function setUpFunction() {
 }
 
 function fillAllTab() {
+    let now = new Date();
+    let bgcolour = 'lightgreen';
+
     let sortedContent = new Map([...content].sort());
+    
     sortedContent.forEach(function(value,key) {
-        let symbol = symbols.get(value.type);
-        let strippedKey = key.replace(/\s/g, '');
-        allTab.innerHTML += '<p id="' + strippedKey + '">' + symbol + ' ' + key[0].toUpperCase() + key.slice(1) + ' ' + 
-        ' ' + value.number + ' stk ' + ' i skuffe ' + value.shelf +  ' </p>'
-    })
+        let symbol = symbols.get(value.type);  // Get the symbol maching the type of food
+        let stock = value.number;
+        let strippedKey = key.replace(/\s/g, '_');  // Replace spaces with underscore
+        strippedKey = strippedKey.replace(/\æ/g, 'ae');
+        strippedKey = strippedKey.replace(/\ø/g, 'oe');
+        strippedKey = strippedKey.replace(/\å/g, 'aa');
+        strippedKey = strippedKey.replace(/\W/g, '');  // Strip non-alphanumeric word characters
+        let daysLeft = value.keepsInMonths * 30 - (now.getTime() - value.addedToFreezer)/(3600000*24);
+        let months = Math.round(value.keepsInMonths - (now.getTime() - value.addedToFreezer)/(3600000*24*30));
+        if (months < 1) {bgcolour = 'yellow'};
+        if (daysLeft < 15) {bgcolour = 'red'};
+
+        allTab.innerHTML += '<div id="' + strippedKey + '" class="itemDiv">' +
+        '<span class="goesLeft">' + symbol + ' ' + key[0].toUpperCase() + key.slice(1) + '</span>' +
+        '<span class="goesRight">' + 
+        '<span style="background-color: ' + bgcolour + '"> ' + months + ' mdr </span>  ' +
+        '&#x2263;' + value.shelf + ' ' + // Four bars symbolizing shelf number
+        '<button class="minus"> &#10134; </button> ' + stock + ' ' + 
+        '<button class="plus"> &#10133; </button> </span> </div>'
+        
+        bgcolour = 'lightgreen';
+    });
 }
